b31dd64838b7882af5f2741a20a8a0ada8f5ce99
Virus01Official <virusidkoff@gmail.com>
2026-04-04 10:51:35 +0200

    first commit

---
+++ scripts/ability_component.gd
@@ -0,0 +1,6 @@
+class_name AbilityComponent
+
+extends Node
+
+func _activate_ability(ability: String) -> void:
+	print(ability)
---
+++ scripts/player.gd
@@ -0,0 +1,69 @@
+extends CharacterBody3D
+
+
+var SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const MOUSE_SENSITIVITY = 0.003
+
+@onready var camera: Camera3D = $Camera3D
+
+@onready var Ability_Component = $Ability_Component
+
+var usingAbility = false
+
+var equippedAbilityOne = "Test"
+var equippedAbilityTwo = "Test"
+
+var pitch: float = 0.0
+
+var MAX_STAMINA = 100.0
+var stamina: float = MAX_STAMINA
+var is_sprinting: bool = false
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("Ability1") and not usingAbility:
+		Ability_Component._activate_ability("Ability1")
+		usingAbility = true
+		await get_tree().create_timer(0.5).timeout
+		abilityTimer_timeout()
+	
+	if Input.is_action_just_pressed("Ability2") and not usingAbility:
+		Ability_Component._activate_ability("Ability2")
+		usingAbility = true
+		await get_tree().create_timer(0.5).timeout
+		abilityTimer_timeout()
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+	
+
+func _input(event: InputEvent) -> void:
+	if not is_multiplayer_authority():
+		return
+		
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+		pitch -= event.relative.y * MOUSE_SENSITIVITY
+		pitch = clamp(pitch, deg_to_rad(-80), deg_to_rad(80))
+		camera.rotation.x = pitch
+		
+func abilityTimer_timeout():
+	usingAbility = false
