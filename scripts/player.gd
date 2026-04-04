0852294227420862a532dfca9e8424e403c47bf0
Virus01Official <virusidkoff@gmail.com>
2026-04-04 13:00:14 +0200

    added cooldowns

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,11 +6,9 @@ var SPEED = 5.0
 const MOUSE_SENSITIVITY = 0.003
 
 @onready var camera: Camera3D = $Camera3D
-
 @onready var Ability_Component = $Ability_Component
 
 var usingAbility = false
-
 var equippedAbilityOne = "Test"
 var equippedAbilityTwo = "Test"
 
@@ -26,36 +24,59 @@ var interact_handlers := {
 	"generator": _interact_generator,
 }
 
+const COOLDOWN_ABILITY1 = 3.0
+const COOLDOWN_ABILITY2 = 5.0
+const COOLDOWN_ATTACK   = 2.0
+
+var cooldowns := {
+	"Ability1": 0.0,
+	"Ability2": 0.0,
+	"Attack":   0.0,
+}
+
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+func _process(delta: float) -> void:
+	for key in cooldowns:
+		if cooldowns[key] > 0.0:
+			cooldowns[key] = max(0.0, cooldowns[key] - delta)
+
+func _is_on_cooldown(action: String) -> bool:
+	return cooldowns.get(action, 0.0) > 0.0
+
+func _start_cooldown(action: String, duration: float) -> void:
+	cooldowns[action] = duration
+
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
-	if Input.is_action_just_pressed("Ability1") and not usingAbility:
+
+	if Input.is_action_just_pressed("Ability1") and not usingAbility and not _is_on_cooldown("Ability1"):
 		Ability_Component._activate_ability("Ability1")
+		_start_cooldown("Ability1", COOLDOWN_ABILITY1)
 		usingAbility = true
 		await get_tree().create_timer(0.5).timeout
 		abilityTimer_timeout()
-	
-	if Input.is_action_just_pressed("Ability2") and not usingAbility:
+
+	if Input.is_action_just_pressed("Ability2") and not usingAbility and not _is_on_cooldown("Ability2"):
 		Ability_Component._activate_ability("Ability2")
+		_start_cooldown("Ability2", COOLDOWN_ABILITY2)
 		usingAbility = true
 		await get_tree().create_timer(0.5).timeout
 		abilityTimer_timeout()
-		
+
 	if Input.is_action_just_pressed("interact") and not usingAbility:
 		var collider = raycast.get_collider()
 		if collider is Area3D:
 			try_interact(collider)
-			
-	if Input.is_action_just_pressed("Attack") and not usingAbility:
+
+	if Input.is_action_just_pressed("Attack") and not usingAbility and not _is_on_cooldown("Attack"):
+		_start_cooldown("Attack", COOLDOWN_ATTACK)
 		usingAbility = true
 		for i in range(5):
 			var spawn_pos = global_position + -transform.basis.z * 1.0
-			spawn_pos.y -= 0.9 
+			spawn_pos.y -= 0.9
 			$"..".add_hitbox(hitboxes, spawn_pos)
 			await get_tree().create_timer(0.05).timeout
 		usingAbility = false
@@ -68,9 +89,8 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
-	
+
 func try_interact(collider: Area3D):
 	if not is_multiplayer_authority():
 		return
@@ -82,15 +102,14 @@ func try_interact(collider: Area3D):
 func _input(event: InputEvent) -> void:
 	if not is_multiplayer_authority():
 		return
-		
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
 		pitch -= event.relative.y * MOUSE_SENSITIVITY
 		pitch = clamp(pitch, deg_to_rad(-80), deg_to_rad(80))
 		camera.rotation.x = pitch
-		
+
 func _interact_generator(collider) -> void:
 	print("gen")
-	
+
 func abilityTimer_timeout():
 	usingAbility = false
